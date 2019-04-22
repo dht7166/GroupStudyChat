8918013e52b3205ba0032e01faa115100149ade2
Hoàn Trần Đức <hdtran@buffalo.edu>
2019-04-22 17:00:46 -0400

    change wss

--- ChatApp/static/js/plugin.js
+++ ChatApp/static/js/plugin.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var socket = new WebSocket('ws://127.0.0.1:8000/chat/');
+	var socket = new WebSocket('wss://intense-eyrie-26763.herokuapp.com/chat/');
 	socket.onopen = websocket_welcome;
 	socket.onmessage = websocket_message_show;
 
